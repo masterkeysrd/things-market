1bbdf0fc51dda279063ffd307a92d886915bd5d5
David Morales <morales.david1997@gmail.com>
2020-12-18 17:35:53 -0400

    refactoring mocker

--- backend/products/tests/mocker.py
+++ backend/products/tests/mocker.py
@@ -1,3 +1,4 @@
+import bson
 from typing import List
 from products.models import Product
 
@@ -8,21 +9,18 @@ WELL_KNOWN_PRODUCT_TYPE = "Phone"
 
 NOT_KNOWN_PRODUCT_ID = "xxxxx80b747cd99d130d1"
 
-def get_mock_product():
-    return Product(
+def get_mock_product(add_id=False, add_attributes=False):
+    product = Product(
         name=WELL_KNOWN_PRODUCT_NAME,
         description=WELL_KNOWN_PRODUCT_DESCRIPTION,
         type=WELL_KNOWN_PRODUCT_TYPE
     )
 
-def get_mock_product_with_id():
-    product = get_mock_product()
-    product.id = WELL_KNOWN_PRODUCT_ID
-    return product
+    if add_id:
+        product.id = WELL_KNOWN_PRODUCT_ID
 
-def get_mock_product_with_attributes():
-    product = get_mock_product()
-    product.attributes = [
+    if add_attributes:
+        product.attributes = product.attributes = [
         {
                'name': 'Display',
                'value': '7.1 inches',
@@ -32,17 +30,25 @@ def get_mock_product_with_attributes():
                'value': '64MP',
            }
     ]
+
     return product
 
-def get_mock_product_list():
+def get_mock_product_list(quantity=10, add_id=False, add_attributes=False):
     product_list: List[Product] = list()
-    product_list.append(get_mock_product())
-    for i in range(9):
-        product_list.append(
-            Product(
+    product_list.append(get_mock_product(add_id, add_attributes))
+
+    for i in range(1, quantity + 1):
+        product = Product(
                 name=f'Product {i}',
                 description=f"Description of product {i}",
-                type=f"Product Type {i}"
-            )
-        )
+                type=f"Product Type {i}")
+        
+        if add_id:
+            product.id=bson.ObjectId()
+
+        if add_attributes:
+            product.attributes= {'name': f'attribute_test {i}', 'value': f'attribute_value {i}'}
+            
+        product_list.append(product)
+        
     return product_list
