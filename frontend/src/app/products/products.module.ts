f080c98f804aad4558fa945959a84514e0e6400c
David Morales <morales.david1997@gmail.com>
2020-12-19 01:17:16 -0400

    Deleting component

--- frontend/src/app/products/products.module.ts
+++ frontend/src/app/products/products.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
-import { ProductListComponent } from './product-list.component';
 import { ProductCreateComponent } from './product-create.component';
 import { RouterModule } from '@angular/router';
 import { PRODUCTS } from './product.routes';
@@ -9,7 +8,7 @@ import { PRODUCTS } from './product.routes';
 
 
 @NgModule({
-  declarations: [ProductComponent, ProductListComponent, ProductCreateComponent],
+  declarations: [ProductComponent, ProductCreateComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(PRODUCTS)
